e9c3727009e468b923ae34b6a9e8b1909de3a5af
ceejayplus <babasolagoodness21@gmail.com>
2025-09-13 04:32:50 +0100

    This page displays the time remaining for a website to be live

---
+++ script.js
@@ -0,0 +1,32 @@
+let countDownDate = new Date("Oct 31, 2025 23:59:59").getTime();
+
+let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+
+    if (distance < 0) {
+        clearInterval(x);
+
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+
+
+        // document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+
+},1000);
