8da2f11b2cdf78c02dab0cc8ec937f8ad5195392
ceejayplus <babasolagoodness21@gmail.com>
2025-11-18 15:05:19 +0100

    changed the time to lauch the wibsite

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date("Oct 31, 2025 23:59:59").getTime();
+let countDownDate = new Date("Oct 31, 2030 23:59:59").getTime();
 
 let x = setInterval(function() {
     let now = new Date().getTime();
